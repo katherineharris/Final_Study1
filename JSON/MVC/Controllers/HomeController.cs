9df7894063fe3a811d84ef55399d51f20f19c009
Katie Harris <katherineharris@ou.edu>
2019-05-09 21:31:57 -0500

    kvh

---
+++ JSON/MVC/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "About the University of Oklahoma Football Program";
+
+            About a = new About();
+
+            return View(a);
+        }
+
+        
+    }
+}
